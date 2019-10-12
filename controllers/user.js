54a232c547fa60b4a344181279a495cc3d8c4c6c
Ever <evercm95@hotmail.com>
2019-10-12 17:30:10 -0500

    project configuration and make enpoint for retrive list of gitHub users

---
+++ controllers/user.js
@@ -0,0 +1,44 @@
+const axios = require('axios')
+
+
+//Endpoint to list Github users with events and gists
+exports.gitHubList= async ({body}, res, next)=>{
+    let responseArray = []
+    for (let username  of  body.usernames){
+        let response = await axios.all([
+            axios.get('https://api.github.com/users/'+username+'/events'),
+            axios.get('https://api.github.com/users/'+username+'/gists')
+        ]).catch(error => {
+            console.log(error)
+          });
+
+        let events = response[0].data
+        let eventsNumber = events.reduce((a, obj) => a + Object.keys(obj).length, 0)
+        
+        if(eventsNumber > 5){
+            events = events.slice(0, 5)
+        }
+
+        let gists = response[1].data
+        let gistsNumber = gists.reduce((a, obj) => a + Object.keys(obj).length, 0)
+
+        if(gistsNumber > 3){
+            gists = gists.slice(0, 3)
+        }
+
+        let userObject = {
+            username: username,
+            events: events,
+            gists: gists
+        }
+        try {
+            for(files of userObject.gists){
+                delete files.files
+            }
+        } catch (error) {
+           return res.status(400).json({error: error})
+        } 
+        responseArray.push(userObject)
+    }
+    return res.status(200).json(responseArray);
+}
---
+++ routes/users.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var users = require('../controllers/user');
+
+
+router.post('/',
+users.gitHubList
+);
+
+module.exports = router;
